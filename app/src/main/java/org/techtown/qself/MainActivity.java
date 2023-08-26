1d71c9ef81c6ada030b3227617f3f4bcf4ce0c04
Caution-Sun <hisbrjseka414>
2023-08-26 19:45:27 +0900

    Init

---
+++ app/src/main/java/org/techtown/qself/MainActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.qself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
